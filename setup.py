8a76ddf595641ef16dacf173136c80f044a069fd
Christoffer Moesgaard <cmoesgaard@gmail.com>
2020-03-13 09:27:15 +0100

    [#35171] Update setup.py and dependencies

--- setup.py
+++ setup.py
@@ -16,23 +16,7 @@ setup(
     author='Magenta ApS',
     author_email='info@magenta.dk',
     license="MPL 2.0",
-    packages=find_packages(exclude=['tests']),
-    package_data={
-        '': ["*.txt", "*.xml"]
-    },
-    zip_safe=False,
     install_requires=[
-        "python3-saml>=1.4",
-        "flask>=1.0",
-        "Flask-Session>=0.3",
-        "requests>=2.19",
-        "Flask-SQLAlchemy>=2.3",
-        "itsdangerous>=1.1",
-        'mock>=3.0.5',
-    ],
-    tests_require=[
-        'Flask-Testing',
-        'freezegun',
-        'werkzeug<1.0.0'
+        'python-dateutil'
     ]
 )
